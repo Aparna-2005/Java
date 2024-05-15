06711d45ff70375b6cb0198c6bff4977b107959b
Aparna-2005 <143735685+Aparna-2005@users.noreply.github.com>
2024-05-15 11:12:05 +0530

    Update palindrome.java

--- Java1/src/com/java1/palindrome.java
+++ Java1/src/com/java1/palindrome.java
@@ -3,12 +3,12 @@ package com.java1;
 public class palindrome {
 public static void main(String[] args) {
 	int a=123,n=0;
-	int res=0;
-	while(a>0){
-		int rem=n%10;
-		res=(res+10)*rem;
-		n=n/10;
-	}
-	System.out.println(res);
+    int res=0;
+    while(a>0){
+        int rem=a%10;
+        res=(res*10)+rem;
+        a=a/10;
+    }
+    System.out.println(res);
 }
 }
